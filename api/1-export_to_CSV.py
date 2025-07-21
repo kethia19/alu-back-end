b6c608706a8ec32cf80753ad6bc40ae8fdfd9d3d
kethia19 <k.kayigire@alustudent.com>
2025-07-21 10:13:24 +0200

    Adding file 1-export_to_CSV.py

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This script retrieves TODO list progress for a given employee ID
+from the JSONPlaceholder API and exports the data to a CSV file.
+"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com"
+
+    # Get user information
+    user_response = requests.get("{}/users/{}".format(url, employee_id))
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
+    username = user_data.get("username")
+
+    # Get TODO list for the employee
+    todos_response = requests.get("{}/todos?userId={}".format(
+        url, employee_id))
+    todos = todos_response.json()
+
+    # Count completed tasks
+    done_tasks = [task for task in todos if task.get("completed")]
+    total_tasks = len(todos)
+    number_of_done_tasks = len(done_tasks)
+
+    # Print results to console
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, number_of_done_tasks, total_tasks))
+    for task in done_tasks:
+        print("\t {}".format(task.get("title")))
+
+    # Export to CSV
+    filename = "{}.csv".format(employee_id)
+    with open(filename, "w", newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([
+                employee_id,
+                username,
+                str(task.get("completed")),
+                task.get("title")
+            ])
